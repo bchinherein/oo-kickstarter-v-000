0655dc56003997e9c105a77fd32fdf18043c6097
Brandon Chin <brandonchin@me.com>
2018-04-03 23:34:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    project.add_backer(self) unless project.backers.include?(backer)
+    project.add_backer(self) unless project.backers.include?(self)
   end
 
 end #class Backer
